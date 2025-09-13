41e89ca8d1e754baa6d907efaa9f8242964d5f14
rokaia1692006 <rokaia.moh.a@gmail.com>
2025-09-13 22:29:59 +0300

    main page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
-
+import draw from "@/components/header";
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      {draw()}
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,68 @@
-import { Text, View } from "react-native";
-
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import Grids from "@/components/grid";
+import { ScrollView } from "react-native";
+interface Item {
+  id: string;
+  title: string;
+  image : any;
+  location: string;
+}
+const DATA: Item[] = [
+  { id: "1", title: "To think of" , image: require('../assets/images/ToThinkOf.png'), location: "Location 1"},
+  { id: "2", title: "Booking a volunteering" , image: require('../assets/images/BookingAVolunteering.png'), location: "Location 2"},
+  { id: "3", title: "checking progress" , image: require('../assets/images/Progress.png'), location: "Location 3"},
+  { id: "4", title: "more about us / contacts" , image: require('../assets/images/MoreAboutUsOrContact.png'), location: "Location 4"},
+];
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>fghjkl ghjkl</Text>
+    <ScrollView>
+   <View style={styles.container}>
+      <ImageBackground source={require('../assets/images/MainPage.jpg')} style={styles.head}>
+        <Text style={styles.headText}>verde</Text>
+      </ImageBackground>
+      <Text style={styles.undertext}>volunteers</Text>
     </View>
+    <View style = {styles.gridcont}>
+    <Grids DATA={DATA} />
+    </View>
+    </ScrollView>
+   
   );
 }
+const styles =  StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  head: {
+   width: "100%", 
+    height: 200,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  headText: {
+    marginBottom: 20,
+     fontSize: 64,
+    fontWeight: "bold",
+    color: "#F5F5F5",
+    textShadowColor: "#000",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 4,
+    textTransform: "uppercase",
+    
+    
+  },
+  undertext: {
+    marginTop: -50,
+    fontStyle: "italic",
+    fontSize: 50,
+    color: "black"
+  },
+  gridcont: {
+    flex: 2,
+    paddingHorizontal: 10,
+    paddingBottom: 20,
+  },
+});
